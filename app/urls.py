1d12a604bb03171e3f8f460faff5c3fe5ff4bc14
F3IT3 <davimadruga@outlook.com.br>
2025-09-13 08:35:28 -0300

    Feat: rotas para views_ataque

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #POKEMONS
     path('',views.listar_pokemons,name='listar_pokemons'),
     path('procurar_pokemon/',views.procurar_pokemon,name='procurar_pokemon'),
     path('criar_pokemon/',views.criar_pokemon,name='criar_pokemon'),
+    path('procurar_troca/<int:pk>/',views.procurar_troca,name='procurar_troca'),
     path('atualizar_pokemon/<int:pk>/',views.atualizar_pokemon,name='atualizar_pokemon'),
     path('deletar_pokemon/<int:pk>/',views.deletar_pokemon,name='deletar_pokemon'),
-    path('procurar_troca/<int:pk>/',views.procurar_troca,name='procurar_troca')
+    #ATAQUES    
+    path('listar_ataques/',views.listar_ataques,name='listar_ataques'),
+    path('procurar_ataque/',views.procurar_ataque,name='procurar_ataque'),
+    path('criar_ataque/',views.criar_ataque,name='criar_ataque'),
+    path('procurar_troca_ataque/<int:pk>/',views.procurar_troca_ataque,name='procurar_troca_ataque'),
+    path('trocar_ataque/<int:pk>/',views.trocar_ataque,name='trocar_ataque'),
+    path('deletar_ataque/<int:pk>/',views.deletar_ataque,name='deletar_ataque'),
 ]
