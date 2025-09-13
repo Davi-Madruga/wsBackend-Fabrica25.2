6bf70dd6f0cd0236256824cb5db52f703523ad4f
F3IT3 <davimadruga@outlook.com.br>
2025-09-13 07:34:09 -0300

    feat: Views(criar_pokemon)

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Pokemon,Ataque
 from .forms import PokemonForm
 from .pokeapi import get_pokemon,get_ataque
@@ -12,7 +12,31 @@ def procurar_pokemon(request):
     return render(request,'procurar_pokemon.html',{'form':form})
 
 def criar_pokemon(request):
-    pass
+    if request.method == 'POST':
+        pokemon = Pokemon()
+        pokemon.id_pokemon = request.POST.get('id_pokemon')
+        pokemon.nome = request.POST.get('nome')
+        pokemon.tipos = request.POST.get('tipos')
+        pokemon.altura = request.POST.get('altura')
+        pokemon.peso = request.POST.get('peso')
+        pokemon.save()
+        return redirect('listar_pokemons')
+    else:
+        form = PokemonForm(request.GET)
+        if form.is_valid():
+            pokemon_name = form.cleaned_data['nome']
+            pokemon_info = get_pokemon(pokemon_name)
+            if pokemon_info:
+                pokemon = Pokemon()
+                pokemon.id_pokemon = pokemon_info['id']
+                pokemon.nome = pokemon_info['name']
+                tipos = '|'
+                for t in pokemon_info['types']:
+                    tipos += f"{t['type']['name']}|"
+                pokemon.tipos = tipos
+                pokemon.altura = pokemon_info['height']
+                pokemon.peso = pokemon_info['weight']
+                return render(request,'criar_pokemon.html',{'pokemon':pokemon})
 
 def atualizar_pokemon(request):
     pass
