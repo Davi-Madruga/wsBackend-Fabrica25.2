f133618f73e3ac0598fe984eddb0fbf476242905
F3IT3 <davimadruga@outlook.com.br>
2025-09-13 07:06:55 -0300

    feat: views futuras

--- app/views.py
+++ app/views.py
@@ -4,3 +4,18 @@ from .models import Pokemon,Ataque
 def listar_pokemons(request):
     pokemons = Pokemon.objects.all().order_by('id_pokemon')
     return render(request, 'listar_pokemons.html',{'pokemons':pokemons})
+
+def procurar_pokemon(request):
+    pass
+
+def criar_pokemon(request):
+    pass
+
+def atualizar_pokemon(request):
+    pass
+
+def procurar_troca(request):
+    pass
+
+def deletar_pokemon(request):
+    pass
