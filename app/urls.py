48547ce17bcc032cb69df6b156a1110cba39388f
F3IT3 <davimadruga@outlook.com.br>
2025-09-13 07:07:24 -0300

    feat: rotas futuras

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.listar_pokemons,name='listar_pokemons'),
+    path('procurar_pokemon/',views.procurar_pokemon,name='procurar_pokemon'),
+    path('criar_pokemon/',views.criar_pokemon,name='criar_pokemon'),
+    path('atualizar_pokemon/<int:pk>/',views.atualizar_pokemon,name='atualizar_pokemon'),
+    path('deletar_pokemon/<int:pk>/',views.deletar_pokemon,name='deletar_pokemon'),
+    path('procurar_troca/<int:pk>/',views.procurar_troca,name='procurar_troca')
 ]
