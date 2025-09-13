30156c94fee8129b4b545a684e3eb607ff6e1963
F3IT3 <davimadruga@outlook.com.br>
2025-09-13 06:57:07 -0300

    Feat: adiçao dos Models 'Pokemon','Ataque'

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    id_pokemon = models.IntegerField()
+    nome = models.CharField()
+    tipos = models.CharField()
+    altura = models.IntegerField()
+    peso = models.IntegerField()
+
+
+    def __str__(self):
+        return f"#{self.id_pokemon}|{self.nome}"
+    
+class Ataque(models.Model):
+    pokemon = models.ForeignKey(Pokemon,on_delete=models.CASCADE)
+    id_ataque = models.IntegerField()
+    nome = models.CharField()
+    tipo = models.CharField()
+    pp = models.CharField()
+    power = models.CharField()
+    accuracy = models.CharField()
+
+    def __str__(self):
+        return self.nome + '(' + self.tipo + ')'
