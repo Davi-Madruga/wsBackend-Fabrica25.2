560696c321bbc386bfad490cf762ab73d253a810
F3IT3 <davimadruga@outlook.com.br>
2025-09-14 09:07:02 -0300

    Fix: Consertando o Container Docker O problema era que quando eu dava o push ia normal mas como clonava o arquivo entry.sh ficava com o EOF=CRLF mas o linux do docker nao entende,adicionei .gitattributes para sempre ir com o EOF=LF

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,3 +3,6 @@ set -e
 python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
+
+#certifique de estar com a LF(Line Feed) 
+#para executar levantar o container
